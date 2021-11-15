a84e441c5eae1b909f77046d81172f9ddef384b3
t3ng4 <94388033+t3ng4@users.noreply.github.com>
2021-11-15 19:40:39 +0100

    Update SaS.sh

--- SaS.sh
+++ SaS.sh
@@ -4,9 +4,9 @@ echo "SimpleAliveScanner"
 is_alive_ping()
 {
   ping -c 1 -w 1 $1 > /dev/null
-  [ $? -eq 0 ] && echo $i ,alive
+  [ $? -eq 0 ] && echo $t ,alive
 }
-for i in 10.8.254.{1..254} #adjust range
+for t in 10.8.254.{1..254} #adjust range
 do
-is_alive_ping $i &
+is_alive_ping $t &
 done
