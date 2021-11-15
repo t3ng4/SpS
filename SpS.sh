604bbd6e63c26507846daf76c75091b501ea6f16
t3ng4 <94388033+t3ng4@users.noreply.github.com>
2021-11-15 17:28:27 +0100

    Add files via upload

---
+++ SpS.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SimplePortScanner-SpS
+echo "SimpePortScanner"
+hostname=10.8.254.171     #ip address of target
+for port in {1..65535};do
+2>/dev/null echo > /dev/tcp/$hostname/$port
+if [ $? == 0 ]
+ then
+ {
+ echo " $port open"
+ }
+fi
+done
